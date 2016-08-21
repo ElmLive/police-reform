4eda5d91336709baa7d7264938313b5f188beb6c
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-08-21 12:44:34 -0700

    Allow zipcode form to be submitted

--- Form.elm
+++ Form.elm
@@ -21,34 +21,71 @@ initialModel =
 
 
 type Msg
-    = ZipCodeChanged String
+    = ZipcodeChanged String
+    | SubmitZipcode
 
 
 update : Msg -> Model -> Model
 update msg model =
     case Debug.log "update" msg of
-        ZipCodeChanged newEntry ->
+        ZipcodeChanged newEntry ->
             { model | zipcodeEntry = newEntry }
+
+        SubmitZipcode ->
+            if Zipcode.isValid model.zipcodeEntry then
+                { model | zipcode = Just model.zipcodeEntry }
+            else
+                model
+
+
+zipcodeForm : String -> Html Msg
+zipcodeForm zipcodeEntry =
+    let
+        zipcodeIsInvalid =
+            not (Zipcode.isValid zipcodeEntry)
+    in
+        Html.form
+            [ class "pure-form pure-form-aligned"
+            , onSubmit SubmitZipcode
+            , disabled zipcodeIsInvalid
+            ]
+            [ fieldset []
+                [ div [ class "pure-control-group" ]
+                    [ label [ for "zipcode" ] [ text "Zipcode" ]
+                    , input
+                        [ id "zipcode"
+                        , type' "text"
+                        , classList
+                            [ ( "input-invalid", zipcodeIsInvalid )
+                            ]
+                        , onInput ZipcodeChanged
+                        ]
+                        []
+                    ]
+                , div [ class "pure-controls" ]
+                    [ button
+                        [ classList
+                            [ ( "pure-button", True )
+                            , ( "pure-button-primary", True )
+                            , ( "pure-button-disabled", zipcodeIsInvalid )
+                            ]
+                        , type' "submit"
+                        , disabled zipcodeIsInvalid
+                        ]
+                        [ text "Continue" ]
+                    ]
+                ]
+            ]
 
 
 view : Model -> Html Msg
 view model =
-    Html.form [ class "pure-form pure-form-aligned" ]
-        [ fieldset []
-            [ div [ class "pure-control-group" ]
-                [ label [ for "zipcode" ] [ text "Zipcode" ]
-                , input
-                    [ id "zipcode"
-                    , type' "text"
-                    , classList
-                        [ ( "input-invalid", not (Zipcode.isValid model.zipcodeEntry) )
-                        ]
-                    , onInput ZipCodeChanged
-                    ]
-                    []
-                ]
-            ]
-        ]
+    case model.zipcode of
+        Nothing ->
+            zipcodeForm model.zipcodeEntry
+
+        Just zipcode ->
+            text "TODO: show police department form"
 
 
 main : Program Never
--- README.md
+++ README.md
@@ -2,8 +2,8 @@ https://www.twitch.tv/avh4
 
  - https://mbylstra.github.io/html-to-elm/
 
- - Entering zip
- - Add CSS for forms
+ - [x] Entering zip
+ - [x] Add CSS for forms
  - Entering police department information
  - Entering police chief information
  - Entering public statements
